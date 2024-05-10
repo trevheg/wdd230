acc51e949c6181003a80bf6024d06a8fdbcd2074
trevheg <trevorhegstrom@gmail.com>
2024-05-09 17:35:05 -0700

    scripture list

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,4 +1,26 @@
 const favChap = document.querySelector('#favchap');
-const addChapter = document.querySelector('button');
+const button = document.querySelector('button');
 const theList = document.querySelector('#list');
 
+button.addEventListener('click', () => {
+    if (favChap.value === '') {
+        favChap.focus();
+    }
+    else {
+        const newList = document.createElement('li');
+        let newButton = document.createElement('button');
+        newButton.textContent = "❌";
+        newButton.addEventListener("click", () => {
+            theList.removeChild(newList);
+            favChap.focus();
+        })
+
+        newList.textContent = favChap.value;
+        newList.appendChild(newButton);
+        theList.appendChild(newList);
+        favChap.focus();
+        favChap.value = '';
+    }
+});
+
+    
